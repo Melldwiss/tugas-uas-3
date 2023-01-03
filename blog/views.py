6914dd2bb2acdc3051a3a00a8d7369bada0874da
Melldwiss <melly.dwisuherlina@gmail.com>
2023-01-03 15:48:36 +0700

    Tugas uas 3

---
+++ blog/views.py
@@ -0,0 +1,18 @@
+from multiprocessing import context
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+from .models import post
+
+def index(request):
+    #query
+    posts = post.objects.all()
+    context = {
+        'title':'tugas uas',
+        'heading':'pertama',
+        'Posts':posts,
+    }
+
+    return render(request,'blog/index.html',context)
